49c426b547daa06306f890f522970b067f96814d
Vm20100 <vincent@TkPadX220v>
2018-02-14 19:59:15 +0100

    Version 1.0 i3 config epure

---
+++ rofi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rofi -combi-modi window,drun,run -show combi -modi combi,run,ssh,drun \
+                -hide-scrollbar \
+         -color-window "#222222, #222222, #b1b4b3" \
+         -color-normal "#222222, #b1b4b3, #222222, #005577, #b1b4b3" \
+         -color-active "#222222, #b1b4b3, #222222, #007763, #b1b4b3" \
+         -color-urgent "#222222, #b1b4b3, #222222, #77003d, #b1b4b3" 
